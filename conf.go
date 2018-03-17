7595b064391447d13d6faf89164cc0ecf0ecf47d
aaculovich <cat@ater.me>
2018-03-17 11:57:26 +0300

    немного срезал выделение памяти, чуть меньше итераций фиклов

--- conf.go
+++ conf.go
@@ -1,6 +1,6 @@
 package main
 
-// наивная реализация. проверка корректности
+// немного срезал аллокаций
 
 import (
 	"bufio"
@@ -10,6 +10,8 @@ import (
 	"io"
 	"net"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -34,6 +36,8 @@ func Fast(inRdr io.Reader, out io.Writer, networks []string) {
 		in          In
 	)
 
+	var netParsed = parseNetworks(networks)
+
 	inRdrBuf := bufio.NewReader(inRdr)
 	for userId := 1; ; userId++ {
 		if line, _, err := inRdrBuf.ReadLine(); err != nil {
@@ -43,14 +47,14 @@ func Fast(inRdr io.Reader, out io.Writer, networks []string) {
 
 			hitsCnt := 0
 
+		hitsLoop:
 			for _, hit := range in.Hits {
 				hitIP := net.ParseIP(string(hit[1 : len(hit)-1]))
 
-				for _, network := range networks {
-					_, networkNet, _ := net.ParseCIDR(network)
-
-					if networkNet.Contains(hitIP) {
-						hitsCnt++
+				for _, network := range netParsed {
+					if !network.Contains(hitIP) {
+					} else if hitsCnt++; hitsCnt >= hitsThresh {
+						break hitsLoop
 					}
 				}
 			}
@@ -58,8 +62,9 @@ func Fast(inRdr io.Reader, out io.Writer, networks []string) {
 			browsersCnt := 0
 
 			for _, browser := range in.Browsers {
-				if userAgentRe.Match(browser) {
-					browsersCnt++
+				if !userAgentRe.Match(browser) {
+				} else if browsersCnt++; browsersCnt >= browsersThresh {
+					break
 				}
 			}
 
@@ -80,4 +85,68 @@ func Fast(inRdr io.Reader, out io.Writer, networks []string) {
 	for _, result := range results {
 		fmt.Fprintln(out, result)
 	}
+}
+
+func parseCIDR(s string) (n net.IPNet) {
+	i := strings.IndexByte(s, '/')
+	addr, maskStr := s[:i], s[i+1:]
+	iplen := net.IPv4len
+	ip := parseIPv4(addr)
+
+	mask, _ := strconv.Atoi(maskStr)
+	m := net.CIDRMask(mask, 8*iplen)
+
+	n.IP = ip.Mask(m)
+	n.Mask = m
+	return
+}
+
+func parseIPv4(s string) net.IP {
+	var p [net.IPv4len]byte
+	for i := 0; i < net.IPv4len; i++ {
+		if len(s) == 0 {
+			// Missing octets.
+			return nil
+		}
+		if i > 0 {
+			if s[0] != '.' {
+				return nil
+			}
+			s = s[1:]
+		}
+		n, c, ok := dtoi(s)
+		if !ok || n > 0xFF {
+			return nil
+		}
+		s = s[c:]
+		p[i] = byte(n)
+	}
+	if len(s) != 0 {
+		return nil
+	}
+	return net.IPv4(p[0], p[1], p[2], p[3])
+}
+
+func dtoi(s string) (n int, i int, ok bool) {
+	const big = 0xFFFFFF
+
+	n = 0
+	for i = 0; i < len(s) && '0' <= s[i] && s[i] <= '9'; i++ {
+		n = n*10 + int(s[i]-'0')
+		if n >= big {
+			return big, i, false
+		}
+	}
+	if i == 0 {
+		return 0, 0, false
+	}
+	return n, i, true
+}
+
+func parseNetworks(netRaw []string) (netParsed []net.IPNet) {
+	netParsed = make([]net.IPNet, len(netRaw))
+	for i, n := range netRaw {
+		netParsed[i] = parseCIDR(n)
+	}
+	return
 }
